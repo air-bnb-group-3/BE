df3eea91a9008db9ce9c82da862180e9d7a2a2d7
aaryadewangga <arya.angga45@gmail.com>
2022-02-23 13:44:05 -0800

    repo test auth

--- repository/auth/auth_test.go
+++ repository/auth/auth_test.go
@@ -1,41 +1,41 @@
 package auth
 
-// import (
-// 	config "app_airbnb/configs"
-// 	"app_airbnb/entities"
-// 	"app_airbnb/repository/user"
-// 	"app_airbnb/utils"
-// 	"testing"
+import (
+	config "app_airbnb/configs"
+	"app_airbnb/entities"
+	"app_airbnb/repository/user"
+	"app_airbnb/utils"
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestLogin(t *testing.T) {
-// 	config := config.GetConfig()
-// 	db := utils.InitDB(config)
-// 	repo := New(db)
+func TestLogin(t *testing.T) {
+	config := config.GetConfig()
+	db := utils.InitDB(config)
+	repo := New(db)
 
-// 	t.Run("success run login", func(t *testing.T) {
-// 		db.Migrator().DropTable(&entities.User{})
-// 		db.AutoMigrate(&entities.User{})
+	t.Run("success run login", func(t *testing.T) {
+		db.Migrator().DropTable(&entities.User{})
+		db.AutoMigrate(&entities.User{})
 
-// 		mockUser := entities.User{Name: "test", Email: "test", Password: "test", Status: "admin"}
-// 		if _, err := user.New(db).Register(mockUser); err != nil {
-// 			t.Fail()
-// 		}
+		mockUser := entities.User{Name: "test", Email: "test", Password: "test", Roles: true}
+		if _, err := user.New(db).Register(mockUser); err != nil {
+			t.Fail()
+		}
 
-// 		mockLogin := entities.User{Email: "test", Password: "test"}
-// 		res, err := repo.Login(mockLogin.Email, mockLogin.Password)
+		mockLogin := entities.User{Email: "test", Password: "test"}
+		res, err := repo.Login(mockLogin.Email, mockLogin.Password)
 
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, "test", res.Email)
-// 		assert.Equal(t, 1, int(res.ID))
-// 	})
+		assert.Nil(t, err)
+		assert.Equal(t, "test", res.Email)
+		assert.Equal(t, 1, int(res.ID))
+	})
 
-// 	t.Run("fail run login", func(t *testing.T) {
-// 		mockLogin := entities.User{Email: "anonim@123", Password: "anonim123"}
-// 		_, err := repo.Login(mockLogin.Email, mockLogin.Password)
-// 		assert.NotNil(t, err)
-// 	})
+	t.Run("fail run login", func(t *testing.T) {
+		mockLogin := entities.User{Email: "anonim@123", Password: "anonim123"}
+		_, err := repo.Login(mockLogin.Email, mockLogin.Password)
+		assert.NotNil(t, err)
+	})
 
-// }
+}
