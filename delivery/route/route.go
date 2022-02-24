9470b3fc8fc1fb367c108a40fbd3a4e7864c8c98
fransihsan <fransihsan8888@gmail.com>
2022-02-24 09:32:44 +0700

    add routes

--- delivery/route/route.go
+++ delivery/route/route.go
@@ -3,6 +3,8 @@ package route
 import (
 	"app_airbnb/delivery/controllers/admin"
 	"app_airbnb/delivery/controllers/auth"
+	"app_airbnb/delivery/controllers/categories"
+	"app_airbnb/delivery/controllers/rooms"
 	"app_airbnb/delivery/controllers/user"
 	"app_airbnb/delivery/middlewares"
 
@@ -10,7 +12,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func RegisterPath(e *echo.Echo, uc *user.UserController, a *admin.AdminController, aa *auth.AuthController) {
+func RegisterPath(e *echo.Echo, uc *user.UserController, a *admin.AdminController, aa *auth.AuthController, r *rooms.RoomsController, cat *categories.CategoriesController) {
 	//CORS
 	e.Use(middleware.CORS())
 
@@ -25,12 +27,26 @@ func RegisterPath(e *echo.Echo, uc *user.UserController, a *admin.AdminControlle
 	e.POST("users/login", aa.Login())
 
 	//ROUTE USERS
-	e.GET("users/me", uc.GetById(), middlewares.JwtMiddleware())
-	e.PUT("users/me", uc.Update(), middlewares.JwtMiddleware())
-	e.DELETE("users/me", uc.Delete(), middlewares.JwtMiddleware())
+	e.GET("/users", uc.GetById(), middlewares.JwtMiddleware())
+	e.PUT("/users", uc.Update(), middlewares.JwtMiddleware())
+	e.DELETE("/users", uc.Delete(), middlewares.JwtMiddleware())
 
 	//ROUTE Admin
 	e.POST("admin/register", a.Register())
 	e.GET("admin/users", a.GetAll(), middlewares.JwtMiddleware())
+
+	// ROUTE Category
+	e.POST("/categories", cat.Insert(), middlewares.JwtMiddleware())
+	e.GET("/categories", cat.GetAll())
+	e.GET("/categories/:categoryid", cat.GetById())
+	e.PUT("/categories/:categoryid", cat.Update(), middlewares.JwtMiddleware())
+	e.DELETE("/categories/:categoryid", cat.Delete(), middlewares.JwtMiddleware())
+
+	//ROUTE Rooms
+	e.POST("/rooms", r.Insert(), middlewares.JwtMiddleware())
+	e.GET("/rooms", r.GetAll())
+	e.GET("/rooms/:roomid", r.GetById())
+	e.PUT("/rooms/:roomid", r.Update(), middlewares.JwtMiddleware())
+	e.DELETE("/rooms/:roomid", r.Delete(), middlewares.JwtMiddleware())
 
 }
--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	config "app_airbnb/configs"
 	_adminController "app_airbnb/delivery/controllers/admin"
 	_authController "app_airbnb/delivery/controllers/auth"
+	_categoryController "app_airbnb/delivery/controllers/categories"
 
 	// _imagesController "app_airbnb/delivery/controllers/images"
-	// _roomsController "app_airbnb/delivery/controllers/rooms"
+	_roomsController "app_airbnb/delivery/controllers/rooms"
 	_userController "app_airbnb/delivery/controllers/user"
 
 	"app_airbnb/delivery/route"
 	_adminRepo "app_airbnb/repository/admin"
 	_authRepo "app_airbnb/repository/auth"
+	_categoryRepo "app_airbnb/repository/categories"
+	_roomsRepo "app_airbnb/repository/rooms"
 	_userRepo "app_airbnb/repository/user"
 
 	"app_airbnb/utils"
@@ -30,18 +33,20 @@ func main() {
 	authRepo := _authRepo.New(db)
 	adminRepo := _adminRepo.New(db)
 	// imageRepo := _imagesRepo.New(db)
-	// roomsRepo := _roomsRepo.New(db)
+	roomsRepo := _roomsRepo.New(db)
+	categoryRepo := _categoryRepo.New(db)
 
 	//CONTROLLER
 	userController := _userController.New(userRepo)
 	authController := _authController.New(authRepo)
 	adminController := _adminController.New(adminRepo)
 	// imagesController := _imagesController.New(imageRepo)
-	// roomsController := _roomsController.New(roomsRepo)
+	roomsController := _roomsController.New(roomsRepo)
+	categoryController := _categoryController.New(categoryRepo)
 
 	e := echo.New()
 
-	route.RegisterPath(e, userController, adminController, authController)
+	route.RegisterPath(e, userController, adminController, authController, roomsController, categoryController)
 
 	log.Fatal(e.Start(fmt.Sprintf(":%d", config.Port)))
 
