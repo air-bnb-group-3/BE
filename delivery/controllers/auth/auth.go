8923ec8939cfee7e7136eb2996f1949d134117b0
aaryadewangga <arya.angga45@gmail.com>
2022-02-27 21:13:15 -0800

    update response login

--- delivery/controllers/auth/auth.go
+++ delivery/controllers/auth/auth.go
@@ -36,7 +36,31 @@ func (ac *AuthController) Login() echo.HandlerFunc {
 		if err != nil {
 			return c.JSON(http.StatusNotAcceptable, common.BadRequest(http.StatusNotAcceptable, "error in process token", nil))
 		}
-		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "berhasil masuk, mendapatkan token baru", token))
+
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "USERS - berhasil masuk, mendapatkan token baru", token))
+
+	}
+}
+
+func (ac *AuthController) AdminLogin() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		Userlogin := LoginReqFormat{}
+
+		if err := c.Bind(&Userlogin); err != nil || Userlogin.Email == "" || Userlogin.Password == "" {
+			return c.JSON(http.StatusBadRequest, common.BadRequest(nil, "error in input file", nil))
+		}
+		checkedUser, err := ac.repo.Login(Userlogin.Email, Userlogin.Password)
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError(nil, "error in call database", nil))
+		}
+		token, err := middlewares.GenerateToken(checkedUser)
+
+		if err != nil {
+			return c.JSON(http.StatusNotAcceptable, common.BadRequest(http.StatusNotAcceptable, "error in process token", nil))
+		}
+
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "ADMIN - berhasil masuk, mendapatkan token baru", token))
 
 	}
 }
